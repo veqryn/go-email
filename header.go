ff85e23a07b974ea19bcb6aeb10a6c1893e61419
Chris Duncan <veqryn@hotmail.com>
2016-02-05 05:30:14 -0700

    work on writing out headers

--- header.go
+++ header.go
@@ -1,13 +1,24 @@
 package email
 
 import (
+	"io"
+	"mime"
 	"net/mail"
 	"net/textproto"
+	"strings"
 	"time"
 )
 
+const (
+	MaxHeaderLengthLength = 78
+	MaxHeaderTotalLength  = 998
+)
+
 // Header represents the key-value MIME-style pairs in a mail message header.
+// Based on textproto.MIMEHeader and mail.Header.
 type Header map[string][]string
+
+// textproto.MIMEHeader Methods:
 
 // Add adds the key, value pair to the header.
 // It appends to any existing values associated with key.
@@ -43,6 +54,8 @@ func (h Header) Del(key string) {
 	delete(h, textproto.CanonicalMIMEHeaderKey(key))
 }
 
+// mail.Header Methods:
+
 // Date parses the Date header field.
 func (h Header) Date() (time.Time, error) {
 	return mail.Header(h).Date()
@@ -51,4 +64,96 @@ func (h Header) Date() (time.Time, error) {
 // AddressList parses the named header field as a list of addresses.
 func (h Header) AddressList(key string) ([]*mail.Address, error) {
 	return mail.Header(h).AddressList(key)
+}
+
+// Methods required for sending a message:
+
+// Save ...
+func (h Header) Save() error {
+	if len(h.Get("Message-Id")) == 0 {
+		id, err := genMessageID()
+		if err != nil {
+			return err
+		}
+		h.Set("Message-Id", id)
+	}
+	if len(h.Get("Date")) == 0 {
+		h.Set("Date", time.Now().Format(time.RFC822))
+	}
+	h.Set("MIME-Version", "1.0")
+	return nil
+}
+
+// WriteTo ...
+func (h Header) WriteTo(w io.Writer) (n int64, err error) {
+	writer := &headerWriter{w: w, maxLineLen: MaxHeaderLengthLength}
+	var total int64
+	// TODO: sort fields (and sort received headers by date)
+	for field, values := range h {
+		for _, val := range values {
+			val = textproto.TrimString(val)
+			for _, s := range []string{field, ": ", mime.QEncoding.Encode("UTF-8", val), "\r\n"} {
+				written, err := io.WriteString(writer, s)
+				if err != nil {
+					return total, err
+				}
+				total += int64(written)
+			}
+			// Reset for next header
+			writer.curLineLen = 0
+		}
+	}
+	return total, nil
+}
+
+// Convenience Methods:
+
+// From ...
+func (h Header) From() string {
+	return h.Get("From")
+}
+
+// SetFrom ...
+func (h Header) SetFrom(email string) {
+	h.Set("From", email)
+}
+
+// To ...
+func (h Header) To() []string {
+	return strings.Split(h.Get("To"), ", ")
+}
+
+// SetTo ...
+func (h Header) SetTo(emails []string) {
+	h.Set("To", strings.Join(emails, ", "))
+}
+
+// Cc ...
+func (h Header) Cc() []string {
+	return strings.Split(h.Get("Cc"), ", ")
+}
+
+// SetCc ...
+func (h Header) SetCc(emails []string) {
+	h.Set("Cc", strings.Join(emails, ", "))
+}
+
+// Bcc ...
+func (h Header) Bcc() []string {
+	return strings.Split(h.Get("Bcc"), ", ")
+}
+
+// SetBcc ...
+func (h Header) SetBcc(emails []string) {
+	h.Set("Bcc", strings.Join(emails, ", "))
+}
+
+// Subject ...
+func (h Header) Subject() string {
+	return h.Get("Subject")
+}
+
+// SetSubject ...
+func (h Header) SetSubject(subject string) {
+	h.Set("Subject", subject)
 }
--- utilities.go
+++ utilities.go
@@ -1,7 +1,14 @@
 package email
 
 import (
+	"bytes"
+	"crypto/rand"
 	"fmt"
+	"io"
+	"math"
+	"math/big"
+	"os"
+	"time"
 )
 
 // interfaceMapToBytesMap ...
@@ -28,4 +35,73 @@ func interfaceToStringMap(m map[string]interface{}) (map[string]string, error) {
 		}
 	}
 	return s, nil
+}
+
+var maxInt64 = big.NewInt(math.MaxInt64)
+
+// genMessageID ...
+func genMessageID() (string, error) {
+	random, err := rand.Int(rand.Reader, maxInt64)
+	if err != nil {
+		return "", nil
+	}
+	hostname, err := os.Hostname()
+	if err != nil {
+		hostname = "localhost"
+	}
+	pid := os.Getpid()
+	nanoTime := time.Now().UTC().UnixNano()
+	return fmt.Sprintf("<%d.%d.%d@%s>", nanoTime, pid, random, hostname), nil
+}
+
+// headerWriter ...
+type headerWriter struct {
+	w          io.Writer
+	curLineLen int
+	maxLineLen int
+}
+
+// Write ...
+func (w *headerWriter) Write(p []byte) (n int, err error) {
+	// TODO: logic for wrapping headers is actually pretty complex for some header types, like received headers
+	var total int
+	for len(p)+w.curLineLen > w.maxLineLen {
+		toWrite := w.maxLineLen - w.curLineLen
+		lastSpace := bytes.LastIndexByte(p[:toWrite], byte(' '))
+		if lastSpace > 0 {
+			toWrite = lastSpace
+		}
+		w.w.Write(p[:toWrite])
+		w.w.Write([]byte("\r\n"))
+		p = p[toWrite:]
+		w.w.Write([]byte(" "))
+		total += toWrite
+		w.curLineLen = 1
+	}
+	w.w.Write(p)
+	w.curLineLen += len(p)
+	return total + len(p), nil
+}
+
+// base64Writer ...
+type base64Writer struct {
+	w          io.Writer
+	curLineLen int
+	maxLineLen int
+}
+
+// Write ...
+func (w *base64Writer) Write(p []byte) (n int, err error) {
+	var total int
+	for len(p)+w.curLineLen > w.maxLineLen {
+		toWrite := w.maxLineLen - w.curLineLen
+		w.w.Write(p[:toWrite])
+		w.w.Write([]byte("\r\n"))
+		p = p[toWrite:]
+		total += toWrite
+		w.curLineLen = 0
+	}
+	w.w.Write(p)
+	w.curLineLen += len(p)
+	return total + len(p), nil
 }
